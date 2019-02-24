b7b60a1a0744740ce1ba3a43417f7e342b4b28dc
Prabuddha <prabuddha.proart@gmail.com>
2019-02-24 23:15:48 +0530

    initial commit with layout fundamentals

---
+++ js/chartjs.js
@@ -0,0 +1,74 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var cty = document.getElementById('myChart-2').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Monthly Enrollement",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 40
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontSize: 30
+                }
+            }]
+        }
+    }
+});
+var chart = new Chart(cty, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["2018", "2019"],
+        datasets: [{
+            label: "Year Enrollement",
+            backgroundColor: 'rgb(255, 99, 32)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 50],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 40
+                }
+            }],
+            xAxes: [{
+                ticks: {
+                    fontSize: 40
+                }
+            }]
+        }
+    }
+});
